eeadffe6e3d1f3d5776e92df73d8357df3ab824a
cohuangyong <1104624312@qq.com>
2018-06-12 17:59:19 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Demo1></Demo1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Demo1 from "./pages/demo1.vue";
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    Demo1
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/demo1.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+    <div class="app">
+        {{message}}
+    </div>
+    <div class="app-bind">
+        <span v-bind:title="message1"> 鼠标悬停几秒钟查看此处动态绑定的提示信息！ </span>
+    </div>
+    <div class="app-for">
+        <ol>
+            <li v-for="todo in todos">
+                {{todo}}
+            </li>
+        </ol>
+    </div>
+    <div class="app-on">
+        <p>{{reverseMsg}}</p>
+        <button v-on:click="reverseMessage"> {{reverseMsg}}</button>
+    </div>
+    <div class="app-model">
+        <p>{{messageModel}}</p>
+<input type="text" v-model="messageModel">
+    </div>
+</div>
+    
+    
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      message: "hello world!",
+      message1: "页面加载于 " + new Date().toLocaleString(),
+      todos: [
+        { text: "学习 JavaScript" },
+        { text: "学习 Vue" },
+        { text: "整个牛项目" }
+      ],
+      reverseMsg: "逆转消息",
+      messageModel: "你好！🍎"
+    };
+  },
+  created() {
+    setInterval(() => {
+      this.message = "hello world!" + new Date().toLocaleString();
+    });
+  },
+  methods: {
+    reverseMessage() {
+      this.reverseMsg = this.reverseMsg
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
+
+
+
+
